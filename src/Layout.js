513caaaa2ffdf7c8d2bee9c11d05cedf7cf0542f
Vian Patel <69381882+VianPatel@users.noreply.github.com>
2023-11-07 18:38:50 -0800

    add tailwind, pages, and navbar

--- src/Layout.js
+++ src/Layout.js
@@ -1,24 +1,37 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useLocation } from "react-router-dom";
 
 
 export default function App() {
+  const location = useLocation().pathname;
+  //<Link class="inline-block py-2 px-4 border-l border-t border-r rounded-t" to="/">Home</Link>
+  //selected class is class="rounded-t-lg overflow-hidden border-t border-l border-r p-4"
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
+      <nav class="pt-4 pl-4 pr-4 flex border-b">
+        <ul class="flex">
+          <li class="mr-6">
+            <SelectorLink selectedLocation={location} to="/" class="inline-block py-2 px-4" classSelected="inline-block py-2 px-4 border-l border-t border-r rounded-t">Home</SelectorLink>
           </li>
-          <li>
-            <Link to="/about">About</Link>
+          <li class="mr-6">
+            <SelectorLink selectedLocation={location} to="/about" class="inline-block py-2 px-4" classSelected="inline-block py-2 px-4 border-l border-t border-r rounded-t">About</SelectorLink>
           </li>
-          <li>
-            <Link to="/projects">Projects</Link>
+          <li class="mr-6">
+            <SelectorLink selectedLocation={location} to="/projects" class="inline-block py-2 px-4" classSelected="inline-block py-2 px-4 border-l border-t border-r rounded-t">Projects</SelectorLink>
           </li>
         </ul>
       </nav>
 
-      <Outlet />
+
+      {/*
+      <div class="pt-4"></div>
+      */}
+
+      <div class="p-4"><Outlet /></div>
     </>
   );
+}
+
+function SelectorLink(props) {
+  //the selected location is the current location
+  return (<Link class={props.selectedLocation == props.to ? props.classSelected : props.class} to={props.to}>{props.children}</Link>);  
 }
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,5 +1,23 @@
 export default function Projects() {
+  
   return (
-    <h1>Projects</h1>
+    <section>
+      <h2 class="pb-4">Projects</h2>
+      <ProjectCard project={project} />
+      <ProjectCard project={project} />
+      <ProjectCard project={project} />
+    </section>
   );
+}
+
+function ProjectCard({project}) {
+  //the selected location is the current location
+  return (
+    <div class="flex items-center space-x-6 p-4">
+    <img class="h-16 w-16 object-cover rounded-full" src={project.image}></img>
+    <a href={project.url}>
+      <h3>{project.name}</h3>
+      <p>{project.description}</p>
+    </a>
+  </div>);
 }

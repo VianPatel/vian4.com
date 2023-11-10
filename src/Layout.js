9c4f00aacca00ccf0a1815885d5fed3e78e8814a
Vian Patel <69381882+VianPatel@users.noreply.github.com>
2023-11-09 16:30:29 -0800

    use pill style for navbar

--- src/Layout.js
+++ src/Layout.js
@@ -7,20 +7,13 @@ export default function App() {
   //selected class is class="rounded-t-lg overflow-hidden border-t border-l border-r p-4"
   return (
     <>
-      <nav class="pt-4 pl-4 pr-4 shadow-md shadow-cyan-600/30 absolute min-w-full bg-cyan-600">
-        <ul class="flex">
-          <li class="mr-6">
-            <SelectorLink selectedLocation={location} to="/" class="text-white inline-block py-2 px-4" classSelected="text-white inline-block py-2 px-4 border-l border-t border-r rounded-t">Home</SelectorLink>
-          </li>
-          <li class="mr-6">
-            <SelectorLink selectedLocation={location} to="/about" class="text-white inline-block py-2 px-4" classSelected="text-white inline-block py-2 px-4 border-l border-t border-r rounded-t">About</SelectorLink>
-          </li>
-          <li class="mr-6">
-            <SelectorLink selectedLocation={location} to="/projects" class="text-white inline-block py-2 px-4" classSelected="text-white inline-block py-2 px-4 border-l border-t border-r rounded-t">Projects</SelectorLink>
-          </li>
-        </ul>
+      <nav class="px-4 py-2 flex shadow-md shadow-cyan-600/30 fixed min-w-full bg-cyan-600">
+        <SelectorLink selectedLocation={location} to="/" class="text-white px-4 py-1" classSelected="text-white inline-block px-4 py-1 border rounded">Home</SelectorLink>
+        <SelectorLink selectedLocation={location} to="/about" class="text-white inline-block px-4 py-1" classSelected="text-white inline-block px-4 py-1 border rounded">About</SelectorLink>
+        <SelectorLink selectedLocation={location} to="/projects" class="text-white inline-block px-4 py-1" classSelected="text-white inline-block px-4 py-1 border rounded">Projects</SelectorLink>
+        <div class="flex-grow"/>
+        <p class="text-white px-4 py-1">Vian4.com</p>
       </nav>
-
 
       {/*
       <div class="pt-4"></div>
@@ -30,7 +23,7 @@ export default function App() {
         <Outlet />
       </div>
 
-      
+
       <div class="p-4 bg-cyan-600">
         <p class="text-white">Footer</p>
       </div>
@@ -41,5 +34,5 @@ export default function App() {
 
 function SelectorLink(props) {
   //the selected location is the current location
-  return (<Link class={props.selectedLocation == props.to ? props.classSelected : props.class} to={props.to}>{props.children}</Link>);  
+  return (<Link class={props.selectedLocation == props.to ? props.classSelected : props.class} to={props.to}>{props.children}</Link>);
 }

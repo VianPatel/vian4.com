499a5b50c086aab698e3ec87461f2e809f4d2fc6
Vian Patel <69381882+VianPatel@users.noreply.github.com>
2023-11-09 22:38:27 -0800

    use justify-center to center PageTextContainer

--- src/components/PageTextContainer.js
+++ src/components/PageTextContainer.js
@@ -1,11 +1,9 @@
 export default function PageTextContainer(props) {
   return (
-    <div class="flex">
-      <div class="flex-grow" />
+    <div class="flex justify-center">
       <div class="flex flex-col w-11/12 sm:w-10/12 md:w-9/12 lg:w-8/12 xl:w-7/12  items-center p-4 border shadow-lg rounded-3xl bg-slate-200">
         {props.children}
       </div>
-      <div class="flex-grow" />
     </div>
   );
 }

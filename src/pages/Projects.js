1676012d9a9714264ce80646c6f890b5510e7be7
Vian Patel <69381882+VianPatel@users.noreply.github.com>
2023-11-09 22:41:01 -0800

    use real projects for list

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,7 +1,7 @@
 const projects = [
   { name: "Thermal Camera Driver", image: "image", description: "Driver connecting mlx90640 thermal sensor to RPI Pico", url: "https://github.com/VianPatel/mlx90640-RPI-Pico" },
-  { name: "Thermal Camera Driver", image: "image", description: "Driver connecting mlx90640 thermal sensor to RPI Pico", url: "https://github.com/VianPatel/mlx90640-RPI-Pico" },
-  { name: "Thermal Camera Driver", image: "image", description: "Driver connecting mlx90640 thermal sensor to RPI Pico", url: "https://github.com/VianPatel/mlx90640-RPI-Pico" },
+  { name: "Grade Calculator ", image: "image", description: "An app assisting in calculating grades", url: "https://github.com/VianPatel/GradeCalculator" },
+  { name: "Air Monitor", image: "image", description: "A program that sends emails when the AQI has passed a configurable threshold. Compatible with an sds011 air quality sensor on a raspberry pi.", url: "https://github.com/VianPatel/AirMonitor" },
 ]
 
 export default function Projects() {

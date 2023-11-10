33d8d663d46f0d55bd4c9e9584c4da1bfe675dd9
Vian Patel <69381882+VianPatel@users.noreply.github.com>
2023-11-09 22:30:19 -0800

    create PageTextContainer

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,12 @@
+import PageTextContainer from "../components/PageTextContainer";
+
 export default function About() {
   return (
     <div>
       <h2 class="pb-4 text-center text-3xl">About</h2>
+      <PageTextContainer>
+        <p>Hi I'm vian</p>
+      </PageTextContainer>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,12 @@
+import PageTextContainer from "../components/PageTextContainer";
+
 export default function Home() {
   return (
     <div>
       <h2 class="pb-4 text-center text-3xl">Home</h2>
+      <PageTextContainer>
+        <p>Welcome to vian4.com</p>
+      </PageTextContainer>
     </div>
   );
 }

377bdcb727a6af5f24cb604de04267a845940d39
Vian Patel <69381882+VianPatel@users.noreply.github.com>
2023-11-15 18:41:44 -0800

    added explainer for clicking on projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,18 +1,22 @@
 import ProjectCard from "../components/ProjectCard";
 
 import PageContainer from "../components/PageContainer";
+import TextContainer from "../components/TextContainer";
 
 import { projects } from "../Config"
 
 export default function Projects() {
   let projectsHTML = projects.map(project => 
-    <div class="pb-4 w-full">
+    <div class="pt-4 w-full">
       <ProjectCard project={project} />
     </div> 
   );
   return (
     <PageContainer>
       <h2 class="pb-4 text-center text-3xl">Projects</h2>
+      <TextContainer>
+        <p>Click a project for more info</p>
+      </TextContainer>
       {projectsHTML}
     </PageContainer>
   );

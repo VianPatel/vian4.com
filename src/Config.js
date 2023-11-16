01121e9e117473b43c78f685a87bfac7b3742a36
Vian Patel <69381882+VianPatel@users.noreply.github.com>
2023-11-15 16:44:35 -0800

    use placeholder images

--- src/Config.js
+++ src/Config.js
@@ -5,20 +5,20 @@ export const footerCopyright = "Copyright © 2023 Vian Patel";
 export const projects = [
     {
         "name": "Thermal Camera Driver",
-        "image": "image",
+        "image": "https://picsum.photos/200",
         "description": "Driver connecting mlx90640 thermal sensor to RPI Pico",
         "url": "https://github.com/VianPatel/mlx90640-RPI-Pico"
     },
     {
         "name": "Grade Calculator ",
-        "image": "image",
+        "image": "https://picsum.photos/200",
         "description": "An app assisting in calculating grades",
         "url": "https://github.com/VianPatel/GradeCalculator"
     },
     {
         "name": "Air Monitor",
-        "image": "image",
+        "image": "https://picsum.photos/200/2000",
         "description": "A program that sends emails when the AQI has passed a configurable threshold. Compatible with an sds011 air quality sensor on a raspberry pi.",
         "url": "https://github.com/VianPatel/AirMonitor"
-    }
+    },
 ];

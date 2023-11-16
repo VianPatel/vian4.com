c54d5f2476e51ae3b283e0834721017fdb4c79db
Vian Patel <69381882+VianPatel@users.noreply.github.com>
2023-11-15 16:53:33 -0800

    remove unneeded div for project image

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -2,12 +2,8 @@ export default function ProjectCard({ project } ) {
   //the selected location is the current location
   return (
     <a href={project.url} class={"flex items-center p-4 space-x-6 border shadow-lg rounded-2xl bg-slate-200 "}>
-      <div class="w-32 h-32 flex-shrink-0">
-
-        <img class="w-full h-full object-cover rounded-lg" src={project.image}></img>
-      </div>
-
-      {/*object-cover */}
+      <img class="w-32 h-32 flex-shrink-0 object-cover rounded-lg" src={project.image}></img>
+      
       <div>
         <h3 class="font-bold">{project.name}</h3>
         <p>{project.description}</p>

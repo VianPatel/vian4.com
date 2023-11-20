a17f724c9175c34b3788bd0bfa71ccbfb8cee8b3
Vian Patel <69381882+VianPatel@users.noreply.github.com>
2023-11-19 17:35:06 -0800

    no longer centering items in text container

--- src/components/TextContainer.js
+++ src/components/TextContainer.js
@@ -1,6 +1,7 @@
 export default function TextContainer(props) {
+  // removed items-center
   return (
-    <div class="flex flex-col w-full items-center p-4 shadow-lg rounded-2xl bg-slate-200">
+    <div class="flex flex-col w-full p-4 shadow-lg rounded-2xl bg-slate-200">
       {props.children}
     </div>
   );

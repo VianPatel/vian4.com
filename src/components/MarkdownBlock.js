9e95b66380c9139845b35d06856767bfb51731a2
Vian Patel <69381882+VianPatel@users.noreply.github.com>
2023-12-12 09:48:00 -0800

    use github like css for markdown

--- src/components/MarkdownBlock.js
+++ src/components/MarkdownBlock.js
@@ -3,10 +3,16 @@ import remarkGEMOJI from 'remark-gemoji';
 import rehypeRAW from 'rehype-raw';
 import Markdown from 'react-markdown';
 
+import 'github-markdown-css';
+
+
 
 export default function MarkdownBlock({ markdown }) {
   return (
-    <Markdown remarkPlugins={[remarkGFM, remarkGEMOJI]} rehypePlugins={[rehypeRAW]}>{markdown}</Markdown>
+    
+    <article class="markdown-body" style={{"background-color": "transparent"}}>
+      <Markdown remarkPlugins={[remarkGFM, remarkGEMOJI]} rehypePlugins={[rehypeRAW]}>{markdown}</Markdown>
+    </article>
   );
 }
 

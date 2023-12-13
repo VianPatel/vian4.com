00f8505a70fd0018e897a21df7ad87792107b9df
Vian Patel <69381882+VianPatel@users.noreply.github.com>
2023-12-12 18:52:41 -0800

    placeholder transparent image for air monitor project

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -6,7 +6,7 @@ export default function ProjectCard({ project } ) {
   //the selected location is the current location
   return (
     <Link to={"/projects/" + project.resourceName} class={"flex items-center p-4 space-x-6 border shadow-lg rounded-2xl bg-slate-200 "}>
-      <img class="w-32 h-32 flex-shrink-0 object-cover rounded-2xl" src={project.image}></img>
+      <img class="w-32 h-32 flex-shrink-0 object-cover rounded-2xl" src={project.image ?? "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNgYAAAAAMAASsJTYQAAAAASUVORK5CYII="}></img>
       
       <div>
         <h3 class="font-bold">{project.name}</h3>

71038b5b44aab3a24f3b7ac9ad78b6de24c463cb
Vian Patel <69381882+VianPatel@users.noreply.github.com>
2025-06-20 17:54:03 -0700

    use prose

--- src/components/MarkdownBlock.js
+++ src/components/MarkdownBlock.js
@@ -3,14 +3,11 @@ import remarkGEMOJI from 'remark-gemoji';
 import rehypeRAW from 'rehype-raw';
 import Markdown from 'react-markdown';
 
-import 'github-markdown-css';
-
-
 
 export default function MarkdownBlock({ markdown }) {
   return (
     
-    <article class="markdown-body" style={{"background-color": "transparent"}}>
+    <article class="prose dark:prose-invert">
       <Markdown remarkPlugins={[remarkGFM, remarkGEMOJI]} rehypePlugins={[rehypeRAW]}>{markdown}</Markdown>
     </article>
   );
